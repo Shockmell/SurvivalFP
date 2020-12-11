9b293941a3c53b1d33486284b06b01bff91f5e87
Abhinav Bajaj <47352951+Shockmell@users.noreply.github.com>
2020-12-11 16:36:26 +0530

    First Commit
    Player movement added

---
+++ Assets/Scripts/Player Scripts/MouseControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class MouseControl : MonoBehaviour
+{
+    [SerializeField]
+    private Transform playerRoot, lookRoot;
+    [SerializeField]
+    private bool invert;
+    [SerializeField]
+    private bool can_Unlock = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
